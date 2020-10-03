aeb11d80224b77efc62247a2eca6742c0cab4d95
DNA5769 <dennisthomas2002@gmail.com>
2020-10-03 05:52:08 +0300

    Challenge 8

--- app.js
+++ app.js
@@ -31,6 +31,11 @@ app.get('/compose', (req, res) => {
   res.render('compose');
 });
 
+app.post('/compose', (req, res) => {
+  let content = req.body.content;
+  console.log(content);
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
